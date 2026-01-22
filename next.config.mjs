73dfb69ad248a6572a17d4362bccade6f5cb77db
Dteeech <marshall.isaac@protonmail.com>
2026-01-22 16:09:45 +0100

    remove instrumentationhook from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // Activer l'instrumentation OpenTelemetry
-  experimental: {
-    instrumentationHook: true,
-  },
 };
 
 export default nextConfig;
